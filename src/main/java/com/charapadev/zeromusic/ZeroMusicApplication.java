ebca61aeb4e44dec8f8267bb46da6d68f26a4239
charapa <eduardocharapa@gmail.com>
2023-07-27 23:07:56 -0300

    integration: added Splunk Enterprise integration for centralized logs

--- src/main/java/com/charapadev/zeromusic/ZeroMusicApplication.java
+++ src/main/java/com/charapadev/zeromusic/ZeroMusicApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZeroMusicApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ZeroMusicApplication.class, args);
 	}
-
 }
